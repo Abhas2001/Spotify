6ea1e65dd700dd854c73eb98db182f6fb07c0f8d
Abhas Kumar <abhassinha98@gmail.com>
2024-08-08 02:17:46 +0530

    new changes

---
+++ my-app/src/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./Home.css";
+
+
+export const Home = () => {
+  return (
+    <div className='Container'>
+      <div className='firstbox'>
+         <div>
+            1
+         </div>
+         <div>
+            1
+         </div>
+         <div>
+            1
+         </div>
+      </div>
+
+      <div className='secbox'>
+         <div>
+            1
+         </div>
+         <div>
+            2
+         </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+
