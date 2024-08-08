5c0711982cc3b6b5b037e23925e5453816d756e2
Abhas Kumar <abhassinha98@gmail.com>
2024-08-09 02:22:43 +0530

    artist

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,34 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css";
+import logo from '../src/images/images.png';
+import home from '../src/images/download.png';
+import search from '../src/images/search.jpeg';
+import clickedhm from '../src/images/click.jpeg';
+import arijit from '../src/images/arijit.jpeg';
+import play from '../src/images/plays.png'
+
 
 
 export const Home = () => {
+
+    const[isclick,setisclick]=useState(0);
   return (
     <div className='Container'>
       <div className='firstbox'>
-         <div>
-            1
+         <div className='spotlogo'>
+            <img className='spologo' src={logo}/>
+            <p>Spotify</p>
          </div>
-         <div>
-            1
+         <div className='logo'onClick={()=> setisclick(isclick==0 ? 1:0)}>
+            { isclick == 0 ?
+         <img className='spologo' src= {home}/>:
+         <img className='clickedlogo' src={clickedhm}/>
+            }
+            <p>Home</p>
          </div>
-         <div>
-            1
-         </div>
-      </div>
-
-      <div className='secbox'>
-         <div>
-            1
-         </div>
-         <div>
-            2
+         <div className='logo'>
+         <img className='spologo' src={search}/>
+            <p>Search</p>
          </div>
       </div>
-
+       <div className='abhas'>
+      <div className='secbox' >
+        <div className='chevron'>
+          1
+         </div>
+         <div className='btns'>
+            <button className='sign'>SignUp</button>
+            <button className='log'
+      >Log in</button>
+         </div>
+      </div>
+      <div className='artist'><h>Popular Artist</h>
+      
+       <div className='total'>
+       <div className='box' >
+        <div className='photo'><img className='artpic' src={arijit}/> <img className='play' src={play}/></div>
+        <div className='artistname'>Arijit Singh</div>
+        <div className='job'>Artist</div>
+       </div>
+       </div>
+      </div>
+      </div>
 
     </div>
+
+    
   )
+
 }
 
 
