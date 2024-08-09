d5aae429a2c8f621cf256d9d0745358d98c1f1fb
Abhas Kumar <abhassinha98@gmail.com>
2024-08-10 03:41:18 +0530

    new changes

---
+++ my-app/src/Album.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Album.css'
+import { useState } from 'react';
+import play from '../src/images/plays.png'
+
+const Album = ({album=[{}]}) => {
+    const[ishover, setishover]=useState(0);
+    const[isShow,setisShow]=useState(0);
+  return (
+    <div className='artist'>
+        <div className='tablehead'>
+        <p>Popular albums</p>
+       
+        <button onClick={()=>setisShow(1)}>{isShow==0? "Show all":""}</button>
+
+        </div>
+      <div className='total'>
+       {album.map((item)=>{return(
+       
+       <div className='box' onMouseEnter={()=>{setishover(true)}} onMouseLeave={()=>{setishover(false)}}>
+        <div className='photo'><img className='artpic' src={item.image}/> <img className={ishover?'show':'hide'} src={play}/></div>
+        <div className='artistname'>{item.name}</div>
+        <div className='job'>{item.artist}</div>
+       </div>
+      
+       )})
+}
+
+</div>
+    </div>
+  )
+}
+
+export default Album
--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -4,30 +4,29 @@ import logo from '../src/images/images.png';
 import home from '../src/images/home.png';
 import search from '../src/images/search.png';
 import clickedhm from '../src/images/clickedhm.png';
-import arijit from '../src/images/arijit.jpeg';
 import play from '../src/images/plays.png'
 import left from '../src/images/left.png';
 import right from '../src/images/right.png';
 import Show from './Show';
+import Album from './Album'
+import backlt from '../src/images/backclicked.png'
 
 
 
 
 export const Home = () => {
-
-    const data =[{image:"https://i.scdn.co/image/ab6761610000e5ebcb6926f44f620555ba444fca",name:"Arijit Singh",artist:"Artist"},
+    const data =[{image:"https://i.scdn.co/image/ab6761610000e5ebcb6926f44f620555ba444fca",name:"Pritam",artist:"Artist"},
     {image:"https://i.scdn.co/image/ab676161000051740261696c5df3be99da6ed3f3",name:"Arijit Singh",artist:"Artist"},
-    {image:"https://i.scdn.co/image/ab6761610000e5ebb19af0ea736c6228d6eb539c",name:"Arijit Singh",artist:"Artist"},
-    {image:"https://i.scdn.co/image/ab6761610000e5eba038d7d87f8577bbb9686bd3",name:"Arijit Singh",artist:"Artist"},
-    {image:"https://i.scdn.co/image/ab6761610000e5ebfc7c542c04b5f7dc8f1b1c16",name:"Arijit Singh",artist:"Artist"},
-    {image:"https://i.scdn.co/image/ab6761610000e5ebc40600e02356cc86f0debe84",name:"Arijit Singh",artist:"Artist"},
-    {image:"https://i.scdn.co/image/ab6761610000e5ebfc7c542c04b5f7dc8f1b1c16",name:"Arijit Singh",artist:"Artist"},
-    {image:"https://i.scdn.co/image/ab6761610000e5ebc40600e02356cc86f0debe84",name:"Arijit Singh",artist:"Artist"},
-    {image:"https://i.scdn.co/image/ab6761610000e5ebfc7c542c04b5f7dc8f1b1c16",name:"Arijit Singh",artist:"Artist"}
-]
-
+    {image:"https://i.scdn.co/image/ab6761610000e5ebb19af0ea736c6228d6eb539c",name:"A.R. Rahman",artist:"Artist"},
+    {image:"https://i.scdn.co/image/ab6761610000e5eba038d7d87f8577bbb9686bd3",name:"Sachin-Jigar",artist:"Artist"},
+    {image:"https://i.scdn.co/image/ab6761610000e5ebfc7c542c04b5f7dc8f1b1c16",name:"Anirudh Ravichander",artist:"Artist"},
+    {image:"https://i.scdn.co/image/ab6761610000e5ebc40600e02356cc86f0debe84",name:"Atif Aslam",artist:"Artist"},
+    ]
+   
     const[isclick,setisclick]=useState(0);
     const[ishover, setishover]=useState(0);
+    const[isShow,setisShow]=useState(0);
+    
   return (
     <div className='Container'>
       <div className='firstbox'>
@@ -50,7 +49,7 @@ export const Home = () => {
        <div className='abhas'>
       <div className='secbox' >
         <div className='chevron'>
-            <div><img className='chev' src={left}/></div>
+            <div><img onClick={()=>{setisShow(0)}} className='chev' src={isShow==1?backlt:left}/></div>
             <div><img className='chev' src={right}/></div>
          </div>
          <div className='btns'>
@@ -59,7 +58,13 @@ export const Home = () => {
       >Log in</button>
          </div>
       </div>
-      <div className='artist'><p>Popular artists</p>
+      <div className='artist'>
+        <div className='tablehead'>
+        <p>Popular artists</p>
+       
+        <button onClick={()=>setisShow(1)}>{isShow==0? "Show all":""}</button>
+
+        </div>
       <div className='total'>
        {data.map((x)=>{return(
        
@@ -74,17 +79,15 @@ export const Home = () => {
 
 </div>
 
+{isShow==1?
 
-<Show user={data} />
-
-
-
+<Show user={data} />:
+<Album  album={data}/>
+}
 
       </div>
       </div>
-
     </div>
-
     
   )
 
