d096370e148f281f1dbb8f4bb4d11577ce49e6d4
Abhas Kumar <abhassinha98@gmail.com>
2024-08-14 02:26:17 +0530

    new changes

--- my-app/src/Search.jsx
+++ my-app/src/Search.jsx
@@ -1,36 +1,73 @@
 import React, { useState } from 'react'
 import Body from './Body';
 import { data } from './data';
+import './Search.css'
 
 
+export const getfilterdata=(query,data)=>{
 
-const Search = (value) => {
-    
-    
-    const[stage,setstage] =useState('');
-    function handlechange(e){
-        setstage(e.target.value)
-
+    if(!query){
+        return data;
     }
+    else
 
-    console.log(JSON.stringify(stage));
+    return data.filter(song=>song.name.includes(query));
 
-   const result = data.filter(function(data){
-    return data.name===JSON.stringify(stage);
-   });
-  
-   console.log(result);
-
-//    console.log(result);
-  return (
-    <div className='bg-zinc-400 w-10 h-10'>
-       <input  onInput={handlechange} className='w-25 h-12 '/>
-         { stage.length>0??
-             <Body value={JSON.stringify(stage)}/>
-
-         }
-    </div>
-  )
 }
 
-export default Search
+
+
+
+const Search = () => {
+    const [query, setquery] = useState("");
+      
+
+
+
+
+    const filtereditems =getfilterdata(query,data);
+   
+   
+  
+    return (
+        <div className='bg-zinc-400 w-10 h-10'>
+            <input  onChange={e=>setquery(e.target.value)} className='w-25 h-12' />
+          { query.length>0 && (
+             
+             <div className='total'>
+             { 
+           filtereditems.map((x)=>{
+               return(
+             
+             <div className='box'>
+               
+              <div className='img'>
+                 {x.name}
+               </div>
+            
+               
+               <div className='name'>
+                 {x.name}
+               </div>
+          
+
+
+               <div className='postion'>
+                {x.name}
+               </div>
+          
+             </div>
+            );
+             })
+             
+}
+           </div>
+          )
+
+          }
+         
+        </div>
+    )
+}
+
+export default Search;
